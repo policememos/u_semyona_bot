7a7601226b9fb17a1716ef274a024c632cd49608
policememos <policememos@gmail.com>
2022-09-20 19:06:22 +0500

    render.com version

--- u_semyona_bot_prod.py
+++ u_semyona_bot_prod.py
@@ -6,11 +6,51 @@ from settings import my_token as my_token
 from settings import id_board
 from settings import todo_list_id
 from settings import bot_key
-from settings import areas_id
 from settings import custom_field_id
 from streets_list import streets_map
 import logging
 from aiogram import Bot, Dispatcher, executor, types
+
+areas_id ={
+'631d9c91b9b1b40281c2b09e': 'Академический',
+'631d9c9d62eac501a94a5e77': 'Широкая Речка',
+'631d9ca05edcb904e747ecd6': 'Заречный',
+'631d9ca5502dff0295ff2080': 'Визовский',
+'631d9caaa22b8101e44cb459': 'Лиственный',
+'631d9caefa21c900cba3f3bd': 'Вокзальный',
+'631d9cb645a10001859fb7c2': 'Новая Сортировка',
+'631d9cb93a98690217fea434': 'Семь Ключей',
+'631d9cbc6da1910205b8e63c': 'Старая Сортировка',
+'631d9cc0b2d0ed014fdf7105': 'поселок Палкино',
+'631d9cc299e7790036a4ceb4': 'Калиновский',
+'631d9cc512be9902ca6e4071': 'Комсомольский',
+'631d9cc7072464027c968f42': 'Втузгородок',
+'631d9ccaf0446400d28ae4cc': 'ЖБИ',
+'631d9ccd284f9a03548ee968': 'Изоплит',
+'631d9ccf95723701ea140093': 'Пионерский',
+'631d9cd26a70e100688e8fd8': 'Шарташский',
+'631d9cd6dff9d20101be96fe': 'Краснолесье',
+'631d9cda5650fe03698fcc6a': 'поселок Совхозный',
+'631d9cdd437099015da0746a': 'Центр',
+'631d9ce1d8a2d401048ad67e': 'Юго-Западный',
+'631d9ce4d673d70196ae3e6d': 'Компрессорный',
+'631d9ce62615d80261b88570': 'Лечебный',
+'631d9ce8e2f7ed0188b51dee': 'Кольцово',
+'631d9ceb39340e0036c629e2': 'Птицефабрика',
+'631d9ced53128100a168cbc5': 'Синие Камни',
+'631d9cf11d1448011fe8bd94': 'Парковый',
+'631d9cf4361ad30091d7ba24': 'Малый Исток',
+'631d9cf724d53d0024e0a0dc': 'Уралмаш',
+'631d9cfa8b92db033aefc80e': 'Эльмаш',
+'631d9cfcb7f585013491db3e': 'Ботанический',
+'631d9cfe55ad830116482551': 'Вторчермет',
+'631d9d03b5720f007e13fb83': 'Елизавет',
+'631d9d051cec840174440181': 'Химмаш',
+'631d9d07a0c2240296b69a3e': 'Нижнеисетский',
+'631d9d0ae2be8b01fe69c408': 'Рудный',
+'631d9d0c29b1fb007736235a': 'Уктусский',
+'631d9d0f277d7a016cd4eb13': 'Южный'
+}
 
 
 class DeliverJob:
